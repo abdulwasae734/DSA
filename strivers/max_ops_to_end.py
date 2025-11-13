831a0dca640fc211178237c2d72f02be3c5a6da0
Abdul Wasae <124011700+abdulwasae734@users.noreply.github.com>
2025-11-14 01:25:45 +0530

    13/11/2025

---
+++ strivers/max_ops_to_end.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxOperations(self, s: str) -> int:
+        ones = 0
+        ans = 0
+        length = len(s)
+
+        for i, pussy in enumerate(s):
+            if pussy == '1':
+                ones += 1
+            else:
+                if i + 1 == length:
+                    ans += ones
+                elif s[i+1] == '1':
+                    
+                    ans += ones
+        return ans
