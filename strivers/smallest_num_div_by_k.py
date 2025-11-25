e489f6a286809cd7ded96005715c83b2f9dcd193
Abdul Wasae <abdulwasae734@gmail.com>
2025-11-25 21:22:46 +0530

    25/11/2025

--- strivers/smallest_num_div_by_k.py
+++ strivers/smallest_num_div_by_k.py
@@ -2,7 +2,8 @@ class Solution:
     def smallestRepunitDivByK(self, k: int) -> int:
         if k%2 == 0 or k%5 == 0:
             return -1
-        
+         
+         
         remainder = 0
         length = 0
         seen = set()
